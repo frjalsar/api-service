903adc4e84e4ce8b40e68a20122a904bad7b68a4
steini96 <steini.tki@gmail.com>
2019-10-14 20:44:30 +0000

    Setti í libs
    Ég bjó til meets folder undir lib og einnig create skjal undir því.

--- routes/meets.js
+++ routes/meets.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const makeCreateMeet = require('../lib/meets/create')
+
 function makeMeetRoute (db) {
   const router = new express.Router()
 
   router.get('/', (req, res, next) => {
     res.send('Kiddi sökkar')
+  })
+
+  router.post('/', (req, res, next) => {
+    const createAthlete = makeCreateMeet(db)
+
+    return createAthlete(req.body, req.user)
+      .then(res.json.bind(res))
+      .catch(next)
   })
 
   return router
